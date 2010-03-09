52907936c8a77ca1cf46f08a00da6562b49db03a
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2010-03-09 01:59:31 +0100

    color mode update

--- init.el
+++ init.el
@@ -264,4 +264,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "gray7" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "microsoft" :family "Consolas")))))
+ '(default ((t (:inherit nil :stipple nil :background "gray7" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "microsoft" :family "Consolas")))))
