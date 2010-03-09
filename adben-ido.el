cab6987bad56ea7a14fad72e3fc7173f982d1494
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2010-03-09 10:14:55 +0100

    autocomplete & ido mode

---
+++ adben-ido.el
@@ -0,0 +1,11 @@
+;;
+;; adben-ido.el
+;; 
+;; Made by Adolfo Benedetti
+;; 
+;; Started on  Tue Mar  9 09:48:26 2010 Adolfo Benedetti
+;; Last update Tue Mar  9 09:48:26 2010 Adolfo Benedetti
+;;
+(require 'ido)
+    (ido-mode t)
+    (setq ido-enable-flex-matching t) ;; enable fuzzy matching
