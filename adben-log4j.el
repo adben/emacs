37a65e6848f5630ca97b8abc36d57d498ccdc3c2
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2010-03-09 21:30:46 +0100

    log4j mode update

--- adben-log4j.el
+++ adben-log4j.el
@@ -1,5 +1,5 @@
 ;;; use log4j-mode when file ends in .log or has log4j at start
-(autoload 'log4j-mode "log4j-mode" "Groovy editing mode." t)
+(autoload 'log4j-mode "log4j-mode" "logs editing mode." t)
 (add-to-list 'auto-mode-alist '("\.log$" . log4j-mode))
 (add-to-list 'interpreter-mode-alist '("log4j" . log4j-mode)
 ;;ToDo buffer-font
