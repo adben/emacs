58e4379557245e2883f93b1ae1b7cd5c11bddffa
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2010-03-10 23:53:15 +0100

    changes in automplete and font(lucida console)

--- init.el
+++ init.el
@@ -274,4 +274,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#191717" :foreground "#D2DEC4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Monaco")))))
+'(default ((t (:inherit nil :stipple nil :background "#191717" :foreground "#D2DEC4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Lucida Console")))))
