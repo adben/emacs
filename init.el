563955059c187ee72e578ea05fe64bba02c51794
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2010-12-14 00:43:29 +0100

    updated emacs configs

--- init.el
+++ init.el
@@ -336,4 +336,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Pragmata")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Monaco")))))
