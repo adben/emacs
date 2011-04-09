f13669085fcbe5f915ae9c8c1063b0bd28c57526
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2011-04-10 01:13:49 +0200

    Hunspell fixed

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 (add-to-list 'exec-path "/usr/local/bin")
 (add-to-list 'exec-path "/opt/local/bin")
+(add-to-list 'load-path "/opt/local/share/hunspell")
 (progn (cd "~/.emacs.d/vendor")
        (normal-top-level-add-subdirs-to-load-path))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -281,6 +282,23 @@
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
 			 ("gnu" . "http://elpa.gnu.org/packages/")
 			 ("technomancy" . "http://repo.technomancy.us/emacs/")))
+;; Use hunspell instead of ispell 
+(setq ispell-program-name "hunspell") 
+(require 'rw-language-and-country-codes) 
+(require 'rw-ispell) 
+(require 'rw-hunspell) 
+(setq ispell-dictionary "nl_NL_hunspell")
+(setq
+ ispell-dictionary-alist
+ '((nil				; default 
+    "[A-Za-z]" "[^A-Za-z]" "[']"
+    t ("-d" "/opt/local/share/hunspell/en_US") nil utf-8)
+   ("es_ES"
+    "[A-Za-z]" "[^A-Za-z]" "[']"
+    t ("-d" "/opt/local/share/hunspell/es_ES") nil utf-8)
+   ("nl_NL_hunspell"
+    "[a-zA-Z\304\326\334\344\366\337\374]" "[^a-zA-Z\304\326\334\344\366\337\374]" "[']"
+    t ("-d" "/opt/local/share/hunspell/nl_NL") nil utf-8)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;Custmoized Settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -289,6 +307,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(rw-hunspell-default-dictionary "nl_NL_hunspell") 
+ '(rw-hunspell-dicpath-list (quote ("/opt/local/share/hunspell"))) 
+ '(rw-hunspell-make-dictionary-menu t) 
+ '(rw-hunspell-use-rw-ispell t) 
  '(bbdb-file "~/.emacs.private/dot-bbdb")
  '(column-number-mode t)
  ;;confluence
@@ -314,7 +336,7 @@
  '(erc-modules (quote (button completion fill irccontrols log netsplit noncommands readonly ring scrolltobottom services smiley track)))
  '(erc-services-mode t)
  '(erc-sound-mode t)
- '(erc-sound-path (quote ("/home/adolfo/.emacs.d/sounds")))
+ '(erc-sound-path (quote ("~/.emacs.d/sounds")))
  '(erc-stamp-mode t)
  '(erc-user-full-name "Adolfo Benedetti")
  '(fill-column 81)
@@ -339,43 +361,6 @@
  '(user-mail-address "adolfo.benedetti@gmail.com")
  '(w3m-default-display-inline-images t))
     ; Double the default size. big logs log4j
-;;ispell configs for Mac OSX. dutch as default, english configured
-;;hunspell
-(setq ispell-program-name "hunspell")
-(setq ispell-dictionary-alist
-  '((nil "[A-Za-z]" "[^A-Za-z]" "[']" t ("-d" "en_US") nil utf-8)))
-(eval-after-load "ispell"
-  '(progn (defun ispell-get-coding-system () 'utf-8)))
-;; ;;aspell 
-;; (setq ispell-program-name "aspell"
-;;       ispell-dictionary-alist
-;;       '(("dutch" "[a-zA-Z\304\326\334\344\366\337\374]"
-;; 	 "[^a-zA-Z\304\326\334\344\366\337\374]" "[']" t
-;; 	 ("-C" "-d" "nederlands" "--dict-dir"
-;; 	  "/Library/Application Support/cocoAspell/aspell-nl-0.50-2")
-;; 	 "~latin1" iso-8859-1)
-;;         (nil
-;; 	 "[A-Za-z]" "[^A-Za-z]" "[']" nil
-;; 	 ("-B" "-d" "english" "--dict-dir"
-;; 	  "/Library/Application Support/cocoAspell/aspell6-en-6.0-0")
-;; 	 nil iso-8859-1)
-;; 	))
-;;The default value is “ispell” which is not what you want. Your aspell has been installed in /usr/local/bin – this directory should be in your ExecPath. If it isn’t, you might have to add it:
-;; (autoload 'ispell-word "ispell"
-;;          "Check the spelling of word in buffer." t)
-;;       (global-set-key "\e$" 'ispell-word)
-;;       (autoload 'ispell-region "ispell"
-;;          "Check the spelling of region." t)
-;;       (autoload 'ispell-buffer "ispell"
-;;          "Check the spelling of buffer." t)
-;;       (autoload 'ispell-complete-word "ispell"
-;;          "Look up current word in dictionary and try to complete it." t)
-;;       (autoload 'ispell-change-dictionary "ispell"
-;;          "Change ispell dictionary." t)
-;;       (autoload 'ispell-message "ispell"
-;;          "Check spelling of mail message or news post.")
-;;       (autoload 'ispell-minor-mode "ispell"
-;;          "Toggle mode to automatically spell check words as they are typed in.")
 ;;Fonting
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
