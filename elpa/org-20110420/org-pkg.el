b24811a2e0df5aaf96e39305e6861554bb21f74f
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2011-04-21 13:36:39 +0200

    updated orgs

--- elpa/org-20110420/org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org" "20110420" "Outline-based notes management and organizer" nil)
---
+++ elpa/org-20110421/org-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org" "20110421" "Outline-based notes management and organizer" nil)
