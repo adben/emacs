dc34253f72a54621159cf0aa6d8c0e8da9ce3eef
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2011-05-13 10:58:42 +0200

    orgs updated, tango for theme

--- elpa/org-20110508/org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org" "20110508" "Outline-based notes management and organizer" nil)
---
+++ elpa/org-20110512/org-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org" "20110512" "Outline-based notes management and organizer" nil)
