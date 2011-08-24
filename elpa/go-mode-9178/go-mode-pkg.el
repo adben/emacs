40ef6fd93a7f8f210cdc1d4b92cc95e063891d53
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2011-08-24 10:21:21 +0200

    Added modes ecb and scala, updated orgs and go

---
+++ elpa/go-mode-9178/go-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "go-mode" "9178"
+  "Major mode for the Go programming language.")
---
+++ elpa/org-20110823/org-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org" "20110823" "Outline-based notes management and organizer" nil)
