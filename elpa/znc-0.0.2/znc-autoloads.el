9474d9333aca29936f0f023067b30047d7638ca6
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2012-06-05 17:06:01 +0200

    fixes conflict of erc-znc emacs24rc

--- elpa/znc-0.0.2/znc-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; znc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (znc-all znc-erc) "znc" "znc.el" (20042 20261))
-;;; Generated autoloads from znc.el
-
-(autoload 'znc-erc "znc" "\
-Connect to a configured znc network
-
-\(fn &optional NETWORK)" t nil)
-
-(autoload 'znc-all "znc" "\
-Connect to all known networks
-
-\(fn &optional DISCONNECT)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("znc-pkg.el") (20042 20261 443971))
-
-;;;***
-
-(provide 'znc-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; znc-autoloads.el ends here
--- elpa/znc-0.0.2/znc-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "znc" "0.0.2" "ZNC + ERC " (quote ((cl "2.2") (erc "5.3"))))
