009f8ae8c6eedfde3bd13291e01e96c4b3ef4bb2
Adolfo Benedetti <benedettivelasquez@ict.eur.nl>
2013-07-15 14:22:39 +0200

    removed egg, due helm conflict

--- adben-egg.el
+++ adben-egg.el
@@ -4,5 +4,6 @@
 ;; Egg is an Emacs interface to git. It's a suite composed of a
 ;; minor-mode and various special-buffers presenting different UIs to
 ;; help the user performing many git operations.
-(require 'edmacro)
-(require 'egg)
+;;(require 'edmacro)
+;;(require 'egg)
+
