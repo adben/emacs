6a3747e08bdfa090e1c4f3f77bf336f1409ede04
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2014-03-05 10:12:35 +0100

    the package rainbow-delimiter is dynamiamically added to the loading path

--- lisp/adben-rainbow-delimiters.el
+++ lisp/adben-rainbow-delimiters.el
@@ -1,7 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; toward balanced and colorful delimiters
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/elpa/rainbow-delimiters-20120428.45/")
 (when (require 'rainbow-delimiters nil 'noerror) 
   (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode))
 
