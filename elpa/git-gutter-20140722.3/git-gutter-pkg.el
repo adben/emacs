7f405237b6e378852fd1888e9868f54bea335b27
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2014-07-30 10:18:39 +0200

    old packages

--- elpa/git-gutter-20140722.3/git-gutter-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-gutter" "20140722.3" "Port of Sublime Text plugin GitGutter" '((cl-lib "0.5") (emacs "24")) :url "https://github.com/syohex/emacs-git-gutter")
--- elpa/haskell-mode-20140724.935/haskell-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "haskell-mode" "20140724.935" "A Haskell editing mode" 'nil)
