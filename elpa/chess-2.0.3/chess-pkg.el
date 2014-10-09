d70f395cc677fa86cdecfa707fec2be9bcb55d13
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2014-10-09 18:29:23 +0200

    updated packages with stable melpa

--- elpa/chess-2.0.3/chess-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "chess" "2.0.3" "Play chess in GNU Emacs" '((cl-lib "0.5")) :keywords '("games") :url "http://elpa.gnu.org/packages/chess.html")
--- elpa/org-plus-contrib-20140728/org-plus-contrib-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-plus-contrib" "20140728" "Outline-based notes management and organizer" 'nil)
