73129d86aae64e3adf684730db184cabae144288
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2014-11-15 20:42:25 +0100

    fixes for helm and cider

--- elpa/ess-20141111.1903/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20141111.1903" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/perspective-20140606.1246/perspective-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "perspective" "20140606.1246" "switch between named \"perspectives\" of the editor" 'nil :url "http://github.com/nex3/perspective-el" :keywords '("workspace" "convenience" "frames"))
