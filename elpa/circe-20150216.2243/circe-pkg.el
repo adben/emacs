1b9b31dbfaa1ca8c25e09a8105d02521ffafc3f5
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2015-02-22 14:47:42 +0100

    updated packages

--- elpa/circe-20150216.2243/circe-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "circe" "20150216.2243" "Client for IRC in Emacs"
-  '((lui "0")
-    (lcs "0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/pabbrev-20150218.1443/pabbrev-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pabbrev" "20150218.1443" "Predictive abbreviation expansion" 'nil)
