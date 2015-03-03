f49516e6415491850b6ee14d8af528bec46b83d1
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2015-03-03 14:25:44 +0100

    loading config update, elpa updates

---
+++ elpa/circe-20150224.4/circe-pkg.el
@@ -0,0 +1,6 @@
+(define-package "circe" "20150224.4" "Client for IRC in Emacs"
+  '((lui "0")
+    (lcs "0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lui-20150224.4/lui-pkg.el
@@ -0,0 +1,5 @@
+(define-package "lui" "20150224.4" "Linewise User Interface"
+  '((tracking "0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
