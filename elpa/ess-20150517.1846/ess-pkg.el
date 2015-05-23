720904cd9ac92d23cf6486ec9d4d7cc85c7cca70
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2015-05-23 17:59:28 +0200

    package management

--- elpa/ess-20150517.1846/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20150517.1846" "Emacs Speaks Statistics" 'nil :url "http://ess.r-project.org")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/ghc-20150522.646/ghc-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ghc" "20150522.646" "Sub mode for Haskell mode" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
