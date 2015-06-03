26217230eafde6fd5c8494546d9d6f8027795ed0
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2015-06-03 14:55:59 +0200

    updated packages

---
+++ elpa/xml-rpc-20150529.832/xml-rpc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xml-rpc" "20150529.832" "An elisp implementation of clientside XML-RPC" 'nil :url "http://github.com/hexmode/xml-rpc-el" :keywords '("xml" "rpc" "network"))
